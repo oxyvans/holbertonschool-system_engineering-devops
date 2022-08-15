3db0b0d6d0b487cc1ae156ecb823c9ed2ac01dbe
oxyvans <benja.1999.g@hotmail.com>
2022-08-14 17:28:18 -0700

    add task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# task 100
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  repl
