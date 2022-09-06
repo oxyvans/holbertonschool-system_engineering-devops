6085050f7d0904eab6b6c732b2219edbb087c5f2
oxyvans <benja.1999.g@hotmail.com>
2022-09-06 16:20:17 +0000

    add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" apiarda """
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    path = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(path + "users/{}".format(argv[1])).json()
+    all = requests.get(path + "todos/", params={"userId":argv[1]}).json()
+
+    c = 0
+    t = 0
+    complit = []
+    for w in all:
+        if w['completed'] == True:
+            c += 1
+            t += 1
+            complit.append(w["title"])
+        else:
+            t += 1
+
+    print("Employee " + users["name"] + " is done with tasks({}/{}):".format(c,t))
+    for task in complit:
+        print("\t {}".format(task))
