2e56d631d65767688f85a50c8de237fa8d77e3c5
oxyvans <benja.1999.g@hotmail.com>
2022-09-06 17:04:25 +0000

    add task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" apiarda """
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    path = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(path + "users/{}".format(argv[1])).json()
+    all = requests.get(path + "todos/", params={"userId":argv[1]}).json()
+
+with open('{}.csv'.format(str(argv[1])), mode='w') as f:
+    writer = csv.writer(f , quotechar='"', quoting=csv.QUOTE_ALL)
+
+    for i in all:
+         writer.writerow([argv[1], users["username"] , i["completed"] , i["title"]])
