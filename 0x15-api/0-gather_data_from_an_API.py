06fd14b7c221b7afed218d9d91afdc4da9c804b5
oxyvans <benja.1999.g@hotmail.com>
2022-09-06 17:23:32 -0700

    add task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,8 +7,7 @@ from sys import argv
 if __name__ == '__main__':
     path = "https://jsonplaceholder.typicode.com/"
     users = requests.get(path + "users/{}".format(argv[1])).json()
-    all = requests.get(path + "todos/", params={"userId":argv[1]}).json()
-
+    all = requests.get(path + "todos/", params={"userId": argv[1]}).json()
     c = 0
     t = 0
     complit = []
@@ -19,7 +18,6 @@ if __name__ == '__main__':
             complit.append(w["title"])
         else:
             t += 1
-
-    print("Employee " + users["name"] + " is done with tasks({}/{}):".format(c,t))
+    print("Employee "+users["name"]+" is done with tasks({}/{}):".format(c, t))
     for task in complit:
         print("\t {}".format(task))
