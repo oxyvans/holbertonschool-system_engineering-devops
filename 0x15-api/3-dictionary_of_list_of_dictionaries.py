ac3265969a06924a48967692472e80f76df222e8
oxyvans <benja.1999.g@hotmail.com>
2022-09-06 19:05:46 -0700

    add task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,14 +9,5 @@ if __name__ == '__main__':
     users = requests.get(path + "users").json()
     all = requests.get(path + "todos").json()
 
-    res = {}
-    t = []
-    for task in all:
-        td = {}
-        td["task"] = task["title"]
-        td["completed"] = task["completed"]
-        td["username"] = users["username"]
-        t.append(td)
-
     with open("{}.json".format(str(argv[1])), mode='w') as f:
         json.dump(res, f)
