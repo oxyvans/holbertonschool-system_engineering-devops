9baab74cfdadb075157c1d2c20a80f9aee52838a
oxyvans <benja.1999.g@hotmail.com>
2022-09-06 19:02:34 -0700

    add task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" apiarda """
+
+import json
+import requests
+
+if __name__ == '__main__':
+    path = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(path + "users").json()
+    all = requests.get(path + "todos").json()
+
+    res = {}
+    t = []
+    for task in all:
+        td = {}
+        td["task"] = task["title"]
+        td["completed"] = task["completed"]
+        td["username"] = users["username"]
+        t.append(td)
+
+    res[argv[1]] = t
+    with open("{}.json".format(str(argv[1])), mode='w') as f:
+        json.dump(res, f)
