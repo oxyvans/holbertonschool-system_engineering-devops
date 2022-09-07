c9eeb1aac284956f6135037c68dafb2b2577fa4b
oxyvans <benja.1999.g@hotmail.com>
2022-09-06 17:29:35 -0700

    add task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,10 +8,11 @@ from sys import argv
 if __name__ == '__main__':
     path = "https://jsonplaceholder.typicode.com/"
     users = requests.get(path + "users/{}".format(argv[1])).json()
-    all = requests.get(path + "todos/", params={"userId":argv[1]}).json()
+    all = requests.get(path + "todos/", params={"userId": argv[1]}).json()
 
-with open('{}.csv'.format(str(argv[1])), mode='w') as f:
-    writer = csv.writer(f , quotechar='"', quoting=csv.QUOTE_ALL)
+    with open('{}.csv'.format(str(argv[1])), mode='w') as f:
+        writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL)
 
     for i in all:
-         writer.writerow([argv[1], users["username"] , i["completed"] , i["title"]])
+        writer.writerow([argv[1], users["username"],
+                        i["completed"], i["title"]])
