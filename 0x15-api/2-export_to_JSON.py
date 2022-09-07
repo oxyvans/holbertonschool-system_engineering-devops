b78e99fbd5116b22a9c8ef85f1b1db88a635f718
oxyvans <benja.1999.g@hotmail.com>
2022-09-06 18:45:23 -0700

    add task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,9 +8,17 @@ from sys import argv
 if __name__ == '__main__':
     path = "https://jsonplaceholder.typicode.com/"
     users = requests.get(path + "users/{}".format(argv[1])).json()
-    all = requests.get(path + "todos/", params={"userId":argv[1]}).json()
-        
+    all = requests.get(path + "todos/", params={"userId": argv[1]}).json()
+
     res = {}
-    with open('{}.json'.format(str(argv[1])), mode='w') as f:
-        """ open """
+    t = []
+    for task in all:
+        td = {}
+        td["task"] = task["title"]
+        td["completed"] = task["completed"]
+        td["username"] = users["username"]
+        t.append(td)
+
+    res[argv[1]] = t
+    with open("{}.json".format(str(argv[1])), mode='w') as f:
         json.dump(res, f)
